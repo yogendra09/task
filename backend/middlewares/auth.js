e5277ae3c7814241967379f13dba98ed5009f12a
yogendra singh <yogendrayadav09@yahoo.com>
2025-01-15 01:01:24 +0530

    update auth middle ware

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,34 +2,32 @@ const jwt = require("jsonwebtoken");
 const ErrorHandler = require("../utils/ErrorHandler");
 const User = require("../models/userModel");
 
-
 exports.isAuthenticated = async (req, res, next) => {
- try {
-    const  token = req.cookies.token || req.headers.Authorization.split(" ")[1];
-   console.log(req.headers.Authorization);
-   
+  try {
+    const token =  req.cookies?.token ||(req.headers.authorization && req.headers.authorization.split(" ")[1]);
+
     if (!token) {
       return next(new ErrorHandler("please login to access the resource", 401));
     }
-  
+
     const { id } = jwt.verify(token, process.env.JWT_SECRET);
     req.id = id;
     next();
- } catch (error) {
-   return next(new ErrorHandler("please login to access the resource", 401));
- }
+  } catch (error) {
+    return next(new ErrorHandler("please login to access the resource", 401));
+  }
 };
 
 exports.isAdmin = async (req, res, next) => {
-    try {
-      console.log(req.id);
-      
+  try {
+    console.log(req.id);
+
     const user = await User.findById(req.id);
-    if (user.role !== "admin" ) {
+    if (user.role !== "admin") {
       return next(new ErrorHandler("You are not an admin", 401));
     }
     next();
   } catch (error) {
-      res.status(401).json({ message: "server error",status:false });
-   }
-}
+    res.status(401).json({ message: "server error", status: false });
+  }
+};
--- frontend/src/constants/apiUrl.js
+++ frontend/src/constants/apiUrl.js
@@ -1,2 +1,2 @@
-// export const BASE_URL = 'http://localhost:4000';
-export const BASE_URL = 'https://task-o08e.onrender.com'
+export const BASE_URL = 'http://localhost:4000';
+// export const BASE_URL = 'https://task-o08e.onrender.com'
