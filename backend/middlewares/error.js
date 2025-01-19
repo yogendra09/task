e536455671f19d9c26411b0b9c2440459e17c0d1
yogendra singh <yogendrayadav09@yahoo.com>
2025-01-19 17:01:16 +0530

    git push

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -2,7 +2,6 @@ exports.generatedErrors = (err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
 
     if(err.name = "MongoServerError" && err.message.includes("E11000 duplicate key")){
-        err.message = "user with this email or phone number address already exists"
     }
     res.status(statusCode).json({
         status:false,
