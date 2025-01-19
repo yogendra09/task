a8e04255b20820f1d088ef02c7f77d5ca371061c
yogendra singh <yogendrayadav09@yahoo.com>
2025-01-19 16:39:21 +0530

    updated url 2

--- backend/config/passport.config.js
+++ backend/config/passport.config.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:4000/auth/google/callback",
+      callbackURL: "https://task-o08e.onrender.com/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       const { id, displayName, emails, photos } = profile;
